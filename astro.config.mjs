fff4a15cc72b7f80feb8fe86d3826bbd33f79b7b
Gixi <gixi.tsx@gmail.com>
2025-05-27 18:22:14 -0300

    actualicé variable DOMAIN

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import vercel from "@astrojs/vercel/serverless";
+import { DOMAIN } from "./src/utils/const";
 
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
   integrations: [sitemap()],
-  site: "https://c173f308-e60b-4dd0-b533-62b33cce9b19.vercel.app",
+  site: DOMAIN,
   devToolbar: { enabled: false },
   output: "server",
   adapter: vercel(),
--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,4 +1,5 @@
 const WHATSAPP_URL: string = "https://wa.me/+5491176294118";
-const DOMAIN: string = "http://localhost:4321";
+const DOMAIN: string = "https://c173f308-e60b.vercel.app";
+// "http://localhost:4321";
 
 export { WHATSAPP_URL, DOMAIN };
