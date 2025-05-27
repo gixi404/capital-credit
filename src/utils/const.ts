00d73a7d8ac2f5582c62b564bcc6aa80cb74c385
Gixi <gixi.tsx@gmail.com>
2025-05-27 20:50:22 -0300

    oculto imagen en pantallas pequeñas

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,5 +1,6 @@
 const WHATSAPP_URL: string = "https://wa.me/+5491176294118";
-const DOMAIN: string = "https://c173f308-e60b.vercel.app";
-// "http://localhost:4321";
+const DOMAIN: string =
+  // "https://c173f308-e60b.vercel.app";
+  "http://localhost:4321";
 
 export { WHATSAPP_URL, DOMAIN };
