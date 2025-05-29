4cec27e720e3e120c07af6d9d51ca2304d0e140d
Gixi <gixi.tsx@gmail.com>
2025-05-29 15:47:08 -0300

    domain actualizado

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,5 @@
-const DOMAIN: string =
-  // "https://c173f308-e60b.vercel.app";
-  "http://localhost:4321";
+const DOMAIN: string = "https://c173f308-e60b.vercel.app";
+// "http://localhost:4321";
 
 const WHATSAPP_URL: string = "https://wa.me/+5491176294118";
 
